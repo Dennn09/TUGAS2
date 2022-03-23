9439c42d87295194c3982fdb3127698fd8650f9a
Deni Masulili <denymasulili16@gmail.com>
2022-03-23 22:48:12 +0800

    TUGAS2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiaplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "Deni";
+            taxi.OnDuty = true;
+            taxi.NumPasangger = 12;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPasangger();
+            taxi.DropOffPasangger();
+            Console.ReadKey();
+
+        }
+    }
+}
